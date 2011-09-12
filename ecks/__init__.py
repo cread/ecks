e065cd45add29040fa8e7d8da2e0dea379c53b58
Chris Read <chris.read@gmail.com>
2011-09-12 21:05:08 +0100

    Add the setuptools bit so we can get registered in the Cheeseshop

--- ecks/__init__.py
+++ ecks/__init__.py
@@ -26,7 +26,7 @@
     limitations under the License.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.1'
 __author__ = 'Chris Read <chris.read@gmail.com>'
 
 from ecks import Ecks
---
+++ setup.py
@@ -0,0 +1,54 @@
+"""
+Installs ecks using setuputils
+
+Run:
+
+  python setup.py install
+
+to install this package using this script. 
+
+To install the latest public release use run:
+
+  pip install ecks
+"""
+
+try:
+  from setuptools import setup, find_packages
+except:
+  from distutils.core import setup, find_packages
+
+import sys
+
+import ecks
+
+required_python_version = '2.6'
+
+if sys.version < required_python_version:
+  print "Ecks requires python %s or later, you have %s" % (required_python_version, sys.version)
+  sys.exit(1)
+
+setup(
+  name = "Ecks",
+  version = ecks.__version__,
+  description = "Easy access to SNMP data",
+  long_description = "A simple way to get data out of a remote machine using SNMP without having to deal with a single MIB or OID.",
+  classifiers = [
+    "Development Status :: 4 - Beta",                                                                                             
+    "Intended Audience :: Developers",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Apache Software License",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Topic :: Internet",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Topic :: System :: Monitoring"
+  ],
+  author = "Chris Read",
+  author_email = "chris.read@gmail.com",
+  url = "http://github.com/cread/ecks",
+  license = "Apache 2.0",
+  packages = find_packages(),
+  keywords = "snmp monitoring",
+  install_requires = "pysnmp >= 4.1.16a"
+)
+
