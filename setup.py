aad72b4273887c5efed870d2aad1161868c84ea1
Chris Read <chris.read@gmail.com>
2013-08-15 16:34:54 -0500

    Works with newer pysnmp versions

--- setup.py
+++ setup.py
@@ -49,6 +49,6 @@ setup(
   license = "Apache 2.0",
   packages = find_packages(),
   keywords = "snmp monitoring",
-  install_requires = "pysnmp == 4.2.2"
+  install_requires = "pysnmp >= 4.2.2"
 )
 
